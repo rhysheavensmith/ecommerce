213a0d12792bb7e0ed5d4d9461e9be0f0a7d2cfe
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2023-08-03 11:56:01 +1200

    added basic shopping cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,31 @@ import Card from "./components/Card";
 import "./App.css";
 
 export default function App() {
+  // Init state for the product and cart
   const [product, setProduct] = useState({});
+  const [cart, setCart] = useState([]);
+  // Fetch the data from the API and set the state to the product object
   useEffect(() => {
     fetch(
       `https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product`
     ).then((res) => res.json().then((data) => setProduct(data)));
   }, []);
+  // Function to update the items in the cart
+  function updateCart(newItem) {
+    setCart((prevCart) => [...prevCart, newItem]);
+  }
+
+  useEffect(() => console.log(cart), [cart]);
 
   return (
     <div className="app">
       <Nav />
-      <Card specs={product} />
+      <Card
+        specs={product}
+        key={product.id}
+        updateCart={updateCart}
+        cart={cart}
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Card({ specs }) {
+export default function Card({ specs, updateCart }) {
   // Check the data has loaded before trying to render the card
   if (!specs || !specs.sizeOptions) {
     return <div>Loading...</div>;
   }
-  // map over the sizes and return a button for eachzz
-  const sizes = specs.sizeOptions.map((size, index) => (
-    <button key={index}>{size.label}</button>
+
+  const [selectedSize, setSelectedSize] = useState(null);
+
+  // map over the sizes and return a button for each
+  const sizes = specs.sizeOptions.map((size) => (
+    <button key={size.id} onClick={() => pickSize(size)}>
+      {size.label}
+    </button>
   ));
+
+  // Function to handle size selection and update size state
+  function pickSize(size) {
+    setSelectedSize(size);
+  }
+
+  // Function to handle adding product to the cart
+  function addToCart() {
+    // Make sure a size has been selected
+    if (selectedSize === null) {
+      console.log("Please select a size");
+      return;
+    }
+    // Create a new item with the product information and selected size
+    const item = { ...specs, selectedSize: selectedSize };
+    // Add the new item to the cart
+    updateCart(item);
+  }
 
   return (
     <div className="card">
@@ -22,7 +45,7 @@ export default function Card({ specs }) {
         <h3>SIZE</h3>
         {sizes}
         <br />
-        <button>ADD TO CART</button>
+        <button onClick={addToCart}>ADD TO CART</button>
       </div>
     </div>
   );
