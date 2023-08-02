ba8c203c7c95ed4073262883594b2982493d6fca
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2023-08-02 21:37:23 +1200

    fix load issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 export default function Card({ specs }) {
-  // map over the sizes and return a button for each
-  const sizes = specs.sizeOptions.map((size) => <button>{size.label}</button>);
+  // Check the data has loaded before trying to render the card
+  if (!specs || !specs.sizeOptions) {
+    return <div>Loading...</div>;
+  }
+  // map over the sizes and return a button for eachzz
+  const sizes = specs.sizeOptions.map((size, index) => (
+    <button key={index}>{size.label}</button>
+  ));
 
   return (
     <div className="card">
